f5339f7f7a794dc6f667ab8726f1d554487c6d28
th3bala <leoalpbala@gmail.com>
2022-03-06 16:29:32 -0600

    using partial fits to show plot for each separate set of hyperparams

--- NeuralNet-2.py
+++ NeuralNet-2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
@@ -27,12 +28,12 @@ class NeuralNet:
         nrows = len(self.processed_data.index)
 
         #divide attributes (X) from class (Y) which is the target that we want to predict
-        X = self.processed_data.iloc[:, 0:(ncols - 1)]
-        y = self.processed_data.iloc[:, (ncols-1)]
+        self.X = self.processed_data.iloc[:, 0:(ncols - 1)]
+        self.y = self.processed_data.iloc[:, (ncols-1)]
 
         #randomly splits training and test data
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            X, y)
+            self.X, self.y)
 
         #Scale data, sklearn documentation says this is important to do for the Multi-Layer Perceptron
         scaler = StandardScaler()
@@ -57,40 +58,50 @@ class NeuralNet:
         num_hidden_layers = [6] #array with the ith element representing the # of neurons in the ith hidden layer.
 
         # Create the neural network and be sure to keep track of the performance metrics
-        # TODO: cycle through activations, learning rates, max iterations
         for activation in activations:
             for rate in learning_rate:
                 for max in max_iterations:
                     print("Creating neural network with " + str(activation) + " activation function, " + str(rate) + " learning rate, " + str(max) + " max iterations.")
                     neural_network = MLPClassifier(activation=activations[0], hidden_layer_sizes=num_hidden_layers, alpha=1e-5,
-                    learning_rate_init=learning_rate[0], max_iter=max_iterations[1], verbose=True)
-                    #Train the model on the training data
-                    neural_network.fit(self.X_train, self.y_train)
-                    #Test the model on the training data
+                    learning_rate_init=learning_rate[0], max_iter=max_iterations[1], verbose=False)
+                    # Train the model on the training data
+                    plot_x = []
+                    plot_y = []
+                    for i in range(0, max - 1):
+                        neural_network.partial_fit(self.X_train, self.y_train, np.unique(self.y))
+                        plot_x.append(i)
+                        accuracy = neural_network.score(self.X_test, self.y_test)
+                        plot_y.append(accuracy)
+                    # neural_network.fit(self.X_train, self.y_train)
+                    # Test the model on the training data
                     y_train_predictions = neural_network.predict(self.X_train)
-                    #Test the model on the test data
+                    # Test the model on the test data
                     y_test_predictions = neural_network.predict(self.X_test)
 
                     rmse = (np.sqrt(mean_squared_error(self.y_train, y_train_predictions)))
                     r2 = r2_score(self.y_train, y_train_predictions)
 
                     print("Training set performance:")
-                    print('RMSE is {}'.format(rmse))
-                    print('R2 score is {}'.format(r2))
-                    print("\n")
+                    print('RMSE is {}, R2 score is {}'.format(rmse, r2))
 
                     rmse = (np.sqrt(mean_squared_error(self.y_test, y_test_predictions)))
                     r2 = r2_score(self.y_test, y_test_predictions)
 
                     print("Test set performance:")
-                    print('RMSE is {}'.format(rmse))
-                    print('R2 score is {}'.format(r2))
+                    print('RMSE is {}, R2 score is {}'.format(rmse, r2))
                     print("\n")
 
                     # TODO: Plot the model history for each model in a single plot
                     # model history is a plot of accuracy vs number of epochs
                     # you may want to create a large sized plot to show multiple lines
                     # in a same figure.
+
+                    graph_r2 = sns.relplot(x = plot_x, y = plot_y)
+                    graph_r2.set_xlabels("Epochs")
+                    graph_r2.set_ylabels("Accuracy")
+
+                    plt.title("Model history for this model")
+                    plt.show()
 
         return 0
 
