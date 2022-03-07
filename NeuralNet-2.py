e92a876fb251fb97fd10f9a5262e0974e5ce4ec4
th3bala <leoalpbala@gmail.com>
2022-03-07 01:05:41 -0600

    all plots rolled into one

--- NeuralNet-2.py
+++ NeuralNet-2.py
@@ -55,10 +55,10 @@ class NeuralNet:
         activations = ['logistic', 'tanh', 'relu']
         learning_rate = [0.01, 0.1]
         max_iterations = [100, 200] # also known as epochs
-        num_hidden_layers = [6] #array with the ith element representing the # of neurons in the ith hidden layer.
+        num_hidden_layers = [[6], [2,3]] #array with the ith element representing the # of neurons in the ith hidden layer.
 
         #total number of networks that will be created
-        num_networks = len(activations) * len(learning_rate) * len(max_iterations) #update this if testing with more than one hidden layer configuration
+        num_networks = len(activations) * len(learning_rate) * len(max_iterations) * len(num_hidden_layers) #update this if testing with more than one hidden layer configuration
         #keeps track of which network is currently being trained
         network_index = 0
 
@@ -66,43 +66,45 @@ class NeuralNet:
         plot_y_list = [[0] for i in range(num_networks)] #For each network, stores the accuracy for each epoch
 
 
-        # Create the neural network and be sure to keep track of the performance metrics
+        # Create the neural network and be sure to keep track of the performance metrics.
+        # This is going to create 3 * 2 * 2 * 2 models, to reduce how long it takes to run, reduce the variety of hyperparameters above.
         for activation in activations:
             for rate in learning_rate:
                 for max in max_iterations:
-                    print("Creating neural network with " + str(activation) + " activation function, " + str(rate) + " learning rate, " + str(max) + " max iterations.")
-                    neural_network = MLPClassifier(activation=activations[0], hidden_layer_sizes=num_hidden_layers, alpha=1e-5,
-                    learning_rate_init=learning_rate[0], max_iter=max_iterations[1], verbose=False)
+                    for topology in num_hidden_layers:
+                        print("Creating neural network with " + str(activation) + " activation function, " + str(rate) + " learning rate, " + str(max) + " max iterations, and hidden layers as follows: " + str(topology))
+                        neural_network = MLPClassifier(activation=activations[0], hidden_layer_sizes=topology, alpha=1e-5,
+                        learning_rate_init=learning_rate[0], max_iter=max_iterations[1], verbose=False)
 
-                    # Train the model on the training data, running each epoch manually by calling partial_fit each iteration of the for loop, instead of calling fit once
-                    for i in range(1, max):
-                        #Update the model with a single iteration (epoch)
-                        neural_network.partial_fit(self.X_train, self.y_train, np.unique(self.y))
-                        #add the current epoch number (x coordinate) to the plot
-                        plot_x_list[network_index].append(i)
-                        #Get the mean accuracy for this epoch (y coordinate) and add it to the plot
+                        # Train the model on the training data, running each epoch manually by calling partial_fit each iteration of the for loop, instead of calling fit once
+                        for i in range(1, max):
+                            #Update the model with a single iteration (epoch)
+                            neural_network.partial_fit(self.X_train, self.y_train, np.unique(self.y))
+                            #add the current epoch number (x coordinate) to the plot
+                            plot_x_list[network_index].append(i)
+                            #Get the mean accuracy for this epoch (y coordinate) and add it to the plot
+                            accuracy = neural_network.score(self.X_test, self.y_test)
+                            plot_y_list[network_index].append(accuracy)
+
+                        # Test the model on the training data
+                        y_train_predictions = neural_network.predict(self.X_train)
+                        # Test the model on the test data
+                        y_test_predictions = neural_network.predict(self.X_test)
+
+                        rmse = (np.sqrt(mean_squared_error(self.y_train, y_train_predictions)))
+                        accuracy = neural_network.score(self.X_train, self.y_train)
+
+                        print("Training set performance:")
+                        print('RMSE is {}, Accuracy is {}'.format(rmse, accuracy))
+
+                        rmse = (np.sqrt(mean_squared_error(self.y_test, y_test_predictions)))
                         accuracy = neural_network.score(self.X_test, self.y_test)
-                        plot_y_list[network_index].append(accuracy)
 
-                    # Test the model on the training data
-                    y_train_predictions = neural_network.predict(self.X_train)
-                    # Test the model on the test data
-                    y_test_predictions = neural_network.predict(self.X_test)
+                        print("Test set performance:")
+                        print('RMSE is {}, Accuracy is {}'.format(rmse, accuracy))
+                        print("\n")
 
-                    rmse = (np.sqrt(mean_squared_error(self.y_train, y_train_predictions)))
-                    accuracy = neural_network.score(self.X_train, self.y_train)
-
-                    print("Training set performance:")
-                    print('RMSE is {}, Accuracy is {}'.format(rmse, accuracy))
-
-                    rmse = (np.sqrt(mean_squared_error(self.y_test, y_test_predictions)))
-                    accuracy = neural_network.score(self.X_test, self.y_test)
-
-                    print("Test set performance:")
-                    print('RMSE is {}, Accuracy is {}'.format(rmse, accuracy))
-                    print("\n")
-
-                    network_index+=1
+                        network_index+=1
 
         # TODO: Plot the model history for each model in a single plot
         # model history is a plot of accuracy vs number of epochs
@@ -111,7 +113,6 @@ class NeuralNet:
 
         for i in range(0, num_networks):
             plt.plot(plot_x_list[i], plot_y_list[i], label = "Network " + str(i))
-        
         
         plt.xlabel("Epochs")
         plt.ylabel("Accuracy")
@@ -122,6 +123,6 @@ class NeuralNet:
 
 
 if __name__ == "__main__":
-    neural_network = NeuralNet("Dry_Bean_Dataset.xlsx") # put in path to your file
+    neural_network = NeuralNet("https://haydeni-6375-datasets.s3.us-east-2.amazonaws.com/Dry_Bean_Dataset.xlsx") # put in path to your file
     neural_network.preprocess()
     neural_network.train_evaluate()
